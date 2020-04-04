66d7dabfe3b571f5906433cac0f29ab2c536dd5d
Rtroman14 <rtroman14@gmail.com>
2020-04-04 18:49:20 -0500

    adjusted scrolling for page.evaluate & return contact list

--- modules/scrollPage/scrollPage.js
+++ modules/scrollPage/scrollPage.js
@@ -1,44 +1,59 @@
-module.exports = async (scriptMode, lastContact, secondLastContact) => {
+module.exports = async (page, googleSheet) => {
+    let scriptMode = googleSheet.scriptMode;
+    let lastContact = googleSheet.lastContact;
+    let secondLastContact = googleSheet.secondLastContact;
+
     try {
         let previousHeight = 0;
-        let currentHeight = document.scrollingElement.scrollHeight;
+        let currentHeight = await page.evaluate("document.scrollingElement.scrollHeight");
         let total = 0;
 
         while (previousHeight < currentHeight) {
             total++;
-            previousHeight = document.scrollingElement.scrollHeight;
-            window.scrollBy(0, previousHeight);
-            await new Promise(resolve => {
+            previousHeight = await page.evaluate("document.scrollingElement.scrollHeight");
+            await page.evaluate(`window.scrollBy(0, ${previousHeight})`);
+            await new Promise((resolve) => {
                 setTimeout(resolve, 3000);
             });
 
             // scroll step up to load contacts list
             if (total === 2) {
-                window.scrollBy({
+                await page.evaluate(`window.scrollBy({
                     top: -850,
-                    behavior: "smooth"
-                });
-                await new Promise(resolve => {
+                    behavior: "smooth",
+                })`);
+                await new Promise((resolve) => {
                     setTimeout(resolve, 3000);
                 });
             }
 
+            // return list of updated contacts
             if (scriptMode === "Update") {
-                let contactHref = document.querySelectorAll(".mn-connection-card__details > a");
+                let contacts = await page.evaluate(extractContactUrls);
 
                 // break while loop if lastContact in DOM
-                contactHref.forEach((url, index) => {
-                    if (url.href === (lastContact || secondLastContact)) {
-                        previousHeight = document.scrollingElement.scrollHeight;
-
-                        return index;
+                contacts.forEach((url, index) => {
+                    if (url === (lastContact || secondLastContact)) {
+                        return contacts.splice(0, index);
                     }
                 });
             }
 
-            currentHeight = document.scrollingElement.scrollHeight;
+            currentHeight = await page.evaluate("document.scrollingElement.scrollHeight");
         }
+
+        // return all contacts
+        return await page.evaluate(extractContactUrls);
     } catch (error) {
         console.log(`Scroll error = ${error}`);
     }
+};
+
+let extractContactUrls = () => {
+    let contactUrls = [];
+    let contacts = document.querySelectorAll(".mn-connection-card__details > a");
+    for (let contact of contacts) {
+        contactUrls.push(contact.href);
+    }
+    return contactUrls;
 };
