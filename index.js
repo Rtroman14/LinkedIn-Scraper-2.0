6c7f98548cf2a3e7deccce5f6c0fb0f8d2cb52ff
Ryan Roman <ryanroman@Ryans-MBP.domain>
2020-08-16 20:20:25 -0600

    updated userAgent

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ let httpRequestCount = 0;
 
         // robot detection incognito - console.log(navigator.userAgent);
         page.setUserAgent(
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537.36"
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36"
         );
 
         let linkedIn =
